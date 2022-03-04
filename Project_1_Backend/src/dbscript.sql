7f6322930379e3ff73b3cfd7da344314cd1140a3
Ian <jayredd11@gmail.com>
2022-03-04 12:27:53 -0500

    Update dbscript.sql

--- Project_1_Backend/src/dbscript.sql
+++ Project_1_Backend/src/dbscript.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE project_1;
 
+
 CREATE TABLE employee_details(employee_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY, first_name varchar(25), last_name varchar(25), address varchar(25), contact varchar(12), email varchar(25), username varchar(25), password varchar(25));
 
 ALTER TABLE employee_details add column role varchar(10);
@@ -10,5 +11,21 @@ CREATE TABLE manager_details(manager_id int PRIMARY KEY GENERATED ALWAYS AS IDEN
 ALTER TABLE manager_details add column role varchar(10);
 
 
-CREATE TABLE reimbursement_details(reimbursment_id int GENERATED ALWAYS AS IDENTITY, employee_id int, reimbursement_amount float(2), reimbursement_reason varchar(250), reimbursement_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (reimbursment_id), FOREIGN KEY(employee_id) REFERENCES employee_details(employee_id));
 
+CREATE TABLE reimbursement_details(reimbursement_id int GENERATED ALWAYS AS IDENTITY, employee_id int, reimbursement_amount float(2), reimbursement_reason varchar(250), reimbursement_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (reimbursement_id), FOREIGN KEY(employee_id) REFERENCES employee_details(employee_id));
+
+ALTER TABLE reimbursement_details add column status varchar(10);
+
+
+CREATE TABLE pending_details(pending_id int GENERATED ALWAYS AS IDENTITY, employee_id int, reimbursement_amount float(2), reimbursement_reason varchar(250), reimbursement_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (pending_id), FOREIGN KEY(employee_id) REFERENCES employee_details(employee_id));
+
+ALTER TABLE pending_details add column status varchar(10);
+
+
+INSERT INTO employee_details(first_name, last_name, role, username, password) VALUES( 'Jack' , 'Harrison', 'employee', 'Jack12', 'hello'); 
+
+
+
+SELECT  reimbursement_id, employee_id, reimbursement_amount, reimbursement_reason, status, reimbursement_date FROM reimbursement_details WHERE status = 'approved';
+
+SELECT  reimbursement_id, employee_id, reimbursement_amount, reimbursement_reason, status, reimbursement_date FROM reimbursement_details WHERE status = 'denied';
