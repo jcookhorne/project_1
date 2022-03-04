8793b681c95657b1d0283dd7befbd98c24f7ce06
Joshua Cookhorne <joshua.cookhorne@revature.net>
2022-03-04 10:39:03 -0500

    exceptions and main server

---
+++ Project_1_Backend/src/main/java/Server/Main_Reimbursement.java
@@ -0,0 +1,15 @@
+package Server;
+
+import io.javalin.Javalin;
+
+public class Main_Reimbursement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Javalin myServer = Javalin.create().start(7070);
+		System.out.println("Server listening at port 7070 . .");
+
+
+	}
+
+}
